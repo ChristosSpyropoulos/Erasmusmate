517747026f582f84f29645b48080c8423178cd58
GeorgeNekws <gtsikriteas@uth.gr>
2017-05-20 12:08:18 +0200

    clean code a little more2

--- home/models.py
+++ home/models.py
@@ -9,3 +9,4 @@ class Post(models.Model):
     post = models.CharField(max_length=500)
     user = models.ForeignKey(User)
     created = models.DateTimeField(auto_now_add=True)
+    #updated = models.DateTimeField(auto_now=True)
