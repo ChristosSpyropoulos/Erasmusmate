b64f99f55ee98f106ae4c341398e0f04f5f0d618
monkeypunk <monkeypunk@debian>
2017-05-26 23:35:12 +0200

    test home OK

--- home/tests.py
+++ home/tests.py
@@ -2,5 +2,54 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.contrib.auth.models import User
+from accounts.models import UserProfile
+from flats.models import FlatProfile
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+class HomeTestCase(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create(username="user1")
+        self.user2 = User.objects.create(username="user2")
+        self.user3 = User.objects.create(username="user3")
+        self.user4 = User.objects.create_user(username="user4", password="secret")
+        self.user5 = User.objects.create(username="user5")
+        self.user6 = User.objects.create(username="user6")
+        self.user7 = User.objects.create_user(username="user7", password="secret")
+
+        self.flat1 = FlatProfile.objects.create(user=self.user1)
+        self.flat2 = FlatProfile.objects.create(user=self.user2)
+        self.flat3 = FlatProfile.objects.create(user=self.user3)
+        self.flat4 = FlatProfile.objects.create(user=self.user4)
+        self.flat5 = FlatProfile.objects.create(user=self.user5)
+        self.flat6 = FlatProfile.objects.create(user=self.user6)
+        self.flat7 = FlatProfile.objects.create(user=self.user7)
+
+    def test_view_home(self):
+        self.client.login(username='user7', password='secret')
+        response1 = self.client.get(reverse('home:home'))
+
+        self.assertEqual(response1.status_code, 200)
+
+        self.assertContains(response1, self.flat7.name)
+        self.assertContains(response1, self.flat6.name)
+        self.assertContains(response1, self.flat5.name)
+        self.assertContains(response1, self.flat4.name)
+        self.assertContains(response1, self.flat3.name)
+
+        self.assertNotContains(response1, self.user7.username)
+        self.assertContains(response1, self.user6.username)
+        self.assertContains(response1, self.user5.username)
+        self.assertContains(response1, self.user4.username)
+        self.assertContains(response1, self.user3.username)
+        self.assertContains(response1, self.user2.username)
+
+        self.client.logout()
+        self.client.login(username='user4', password='secret')
+        response2 = self.client.get(reverse('home:home'))
+        self.assertContains(response2, self.user7.username)
+        self.assertContains(response2, self.user6.username)
+        self.assertContains(response2, self.user5.username)
+        self.assertNotContains(response2, self.user4.username)
+        self.assertContains(response2, self.user3.username)
+        self.assertContains(response2, self.user2.username)
