97f84e3ef85ffed4d45376a6d0d4ce36f817413c
monkeypunk <monkeypunk@debian>
2017-05-29 20:28:59 +0200

    static settings

--- Erasmusmates/settings.py
+++ Erasmusmates/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+ru^-az!8_zi%ap1&a)ypvy!r9i3i8s^@+(=vc!*&bpt=(q%+@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
