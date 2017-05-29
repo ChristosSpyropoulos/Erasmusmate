4d23bb21e3634b3a8dee2f2feabe075a5552cdf8
monkeypunk <monkeypunk@debian>
2017-05-29 23:02:20 +0200

    static settings

--- Erasmusmates/urls.py
+++ Erasmusmates/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include, patterns
+from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
 from django.conf import settings
@@ -27,7 +27,3 @@ urlpatterns = [
     url(r'^flats/', include('flats.urls', namespace='flats')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += patterns('',
- (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
- )
